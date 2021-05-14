3a0996432897ebf9e7e0e8be0759cc26c8da00a0
nandithaembar-sentry <82904656+nandithaembar-sentry@users.noreply.github.com>
2021-05-14 10:08:39 -0700

    includes pagination

--- create_metric_alert/spotify_script.py
+++ create_metric_alert/spotify_script.py
@@ -25,7 +25,8 @@ import time
 #Plan 1 - slack
 # dedicated slack channel - anyone can be added here
 
-sleep_value = 0
+alert_list = []
+proj_list = []
 
 def get_datetime():
     date_format='%m-%d-%Y_%I:%M:%S %Z'
@@ -113,63 +114,151 @@ def check_threshold(critical_threshold, warning_threshold):
     else:
         logging.info('this is correct')
 
-def check_alert(org_name, auth_key):
+# def check_alert(org_name, auth_key):
 
-    alert_list = []
-    response = requests.get(
-            f'https://sentry.io/api/0/organizations/{org_name}/alert-rules/',
-            headers={'Authorization': auth_key,
-                    'Content-Type' : 'application/json'})
-    json_data = response.json()
-    #print(json_data)
+#     alert_list = []
+#     response = requests.get(
+#             f'https://sentry.io/api/0/organizations/{org_name}/alert-rules/',
+#             headers={'Authorization': auth_key,
+#                     'Content-Type' : 'application/json'})
+#     json_data = response.json()
+#     #print(json_data)
+#     for item1 in json_data:
+#         try:
+#             #print(item1["id"])
+#             #print(item1["name"])
+#             alert_list.append(item1["name"])
+#         except:
+#             logging.error('create_alert: could not get exisitng alert name')
+#     print(alert_list)
+
+#     return alert_list
+
+
+
+def store_data(org_name, data_source, json_data, list_to_update):
+
     for item1 in json_data:
         try:
             #print(item1["id"])
             #print(item1["name"])
-            alert_list.append(item1["name"])
+            list_to_update.append(item1["name"])
         except:
-            logging.error('create_alert: could not get exisitng alert name')
-    print(alert_list)
+            logging.error('could not get exisitng ' + {data_source} + ' name')
+    print(list_to_update)
 
-    return alert_list
-
-def create_alerts(org_name, auth_key, team_id, alert_list, sleep_time, critical_threshold, warning_threshold):
-    # ct = critical_threshold
-    # print(warning_threshold)
-    # gets projects names from org and writes to proj_list
-    proj_list = []
-    response = requests.get(
-            f'https://sentry.io/api/0/organizations/{org_name}/projects/',
-            headers={'Authorization': auth_key,
-                    'Content-Type' : 'application/json'})
-        # if there is response data then try converting it to json
-   # print(response.status_code)
-    if(response.status_code == 200 and len(response.text) > 0):
-        try: #checking if response has data to convert
-            json_data = response.json()
-        except:# if json conversion fails 
-            logging.error('create_alert: could not convert http response to json object')
-            sys.exit()
-    else:
-        logging.error('create_alert: HTTP request failed. Status : ' + str(response.status_code) )
+def get_data(org_name, data_source, list_to_update): 
+    global alert_list
+    try: 
+        response = requests.get(
+                f' https://sentry.io/api/0/organizations/{org_name}/{data_source}/',
+                headers={'Authorization': 'Bearer f363a496d7834eae9805c6e8772818e44c7da041e013488da83a3328e03e52a7',
+                        'Content-Type' : 'application/json'})
+    except:
+        print('unable to do get request')
         sys.exit()
 
-    for item2 in json_data:
-        try:
-            proj_list.append(item2["name"])
-        except:
-            logging.error('create_alert: could not get project name from json object')
-    print(proj_list)
- 
-    alert_creation_count = 1
+    try: 
+        
+        # header = response.headers
+        print(response.headers["link"])
+        link = response.headers["link"]
+        split_link = link.split(';')[-2]
+        
+        # print(split_link[10:-1])
+        results_tf = split_link[10:-1] 
+        print('HERE')
+        print(results_tf)
+        # call method here. 
+        #if false everything will be appended to list after first call
+        store_data(org_name, data_source, response.json(), list_to_update)
+
+        if (results_tf == "true"):
+            cursor = link.split(';')[-1]
+            cursor = cursor[9:-1]
+            print(cursor)
+
+            while link is not None:
+                
+                try:
+                    #print(f'https://sentry.io/api/0/organizations/testorg-az/alert-rules/?&cursor={cursor}')
+                    response = requests.get(
+                    f' https://sentry.io/api/0/organizations/{org_name}/{data_source}/?&cursor={cursor}',
+                    headers={'Authorization': 'Bearer ef4ced8eee04494681041f979428245001f71a86013046c1a0632fedbea25462',
+                            'Content-Type' : 'application/json'})
+                    print('check here!!')
+                    print(response.headers)
+                    #store_data(org_name, data_source, response.json(), list_to_update)
+                    try:
+                        
+                        link = response.headers["link"]
+                    except:
+                        # that means everything has been appended
+                        print('could not get link')
+                        link = None
+                        return
+
+                    print(link)
+                    split_link = link.split(';')[-2]
+
+                    # print(split_link[10:-1])
+                    results_tf = split_link[10:-1] 
+                    # print(results_tf)
+                    # print('CHECK HERE')
+                    if (results_tf == "true"):
+                        cursor = link.split(';')[-1]
+                        cursor = cursor[9:-1]
+                        print(cursor)
+                    else:
+                        print('setting link to none')
+                        link = None # while loop needs to be none if false
+                except:
+                    print("got exception in inner loop")
+                    sys.exit()
+
+            
+        else:
+            print('extracted all alerts')
+    
+    except: 
+        print('unable to get link')
+
+
+def create_alerts(org_name, auth_key, team_id, sleep_time, critical_threshold, warning_threshold, proj_list, alert_list):
+    
+    # gets projects names from org and writes to proj_list
+    # proj_list = []
+    # response = requests.get(
+    #         f'https://sentry.io/api/0/organizations/{org_name}/projects/',
+    #         headers={'Authorization': auth_key,
+    #                 'Content-Type' : 'application/json'})
+    #     # if there is response data then try converting it to json
+   
+    # if(response.status_code == 200 and len(response.text) > 0):
+    #     try: #checking if response has data to convert
+    #         json_data = response.json()
+    #     except:# if json conversion fails 
+    #         logging.error('create_alert: could not convert http response to json object')
+    #         sys.exit()
+    # else:
+    #     logging.error('create_alert: HTTP request failed. Status : ' + str(response.status_code) )
+    #     sys.exit()
+
+    # for item2 in json_data:
+    #     try:
+    #         proj_list.append(item2["name"])
+    #     except:
+    #         logging.error('create_alert: could not get project name from json object')
+    # print(proj_list)
+
 
     for proj_name in proj_list:
-        alert_name = proj_name + '_quota_limit_0512_testing'
+        alert_name = proj_name + '_quota_limit_0513_123'
 
         if alert_name not in alert_list:
 
             try:
-                # check for 1 horu
+                # check for 1 hour
                 # print(critical_threshold)
                 json_data=jsons.dumps({"dataset":"events","eventTypes":["error","default"],"aggregate":"count()","query":"","timeWindow":60,"triggers":[{"label":"critical","alertThreshold":10000,"actions":[{"unsavedId":"ccbcf983-079a-0703-f2a6-ff12d2eb363d","unsavedDateCreated":"2021-05-07T04:35:29.966Z","type":"email","targetType":"user","targetIdentifier":"1005877","options":None}]},{"label":"warning","alertThreshold":8000,"actions":[{"unsavedId":"ccbcf983-079a-0703-f2a6-ff12d2eb363d","unsavedDateCreated":"2021-05-07T04:35:29.966Z","type":"email","targetType":"user","targetIdentifier":"1005877","options":None}]}],"projects" :[proj_name],"environment": None,"resolveThreshold": None,"thresholdType":0,"owner": None,"name":alert_name})
             except:
@@ -177,11 +266,11 @@ def create_alerts(org_name, auth_key, team_id, alert_list, sleep_time, critical_
                 sys.exit()
                 
             response = requests.post(
-                f'https://sentry.io/api/0/projects/nanditha-test-org/{proj_name}/alert-rules/',
-                headers={'Authorization': auth_key,
+                f'https://sentry.io/api/0/projects/{org_name}/{proj_name}/alert-rules/',
+                headers={'Authorization': 'Bearer bd936f4789324c7a918104ccbfe7482d052d603f3ed946f0a7e837bc0da2602f',
                         'Content-Type' : 'application/json'}, data=json_data)
             if(response.status_code == 200 or response.status_code == 201):
-                logging.info('Successfully created the metric alert for project: ' + proj_name)
+                logging.info('Successfully created the metric alert ' + alert_name + ' for project: ' + proj_name)
             elif (response.status_code == 400):
                 print(response.status_code)
                 logging.error('create_alert: could not create alert for project: ' + proj_name)
@@ -195,52 +284,29 @@ def create_alerts(org_name, auth_key, team_id, alert_list, sleep_time, critical_
             logging.info('alert already exists for project ' + proj_name + '!')
 
   
-
-# def usage():
-#     print('how to use')
-
-# def parse_args(argv):
-#     global sleep_value
-#     #parsing command line arguments
-#     try:
-#         opts, args = getopt.getopt(argv, "ho:s:v", ["help", "output=", "sleep="])
-#     except getopt.GetoptError as err:
-#         # print help information and exit:
-#         print(err)  # will print something like "option -a not recognized"
-#         usage()
-#         sys.exit(2)
-#     output = None
-#     verbose = False
-#     for o, a in opts:
-#         if o == "-v":
-#             verbose = True
-#         elif o in ("-h", "--help"):
-#             usage()
-#             sys.exit()
-#         elif o in ("-o", "--output"):
-#             output = a
-#         elif o in ("-s", "--sleep"):
-#             sleep_value = int(a)/1000.0 # user to assign new sleep value using -s 
-#         else:
-#             assert False, "unhandled option"
-#     # ...
-
  
 def main(argv):
 
     # parse_args(argv)
+    global alert_list
+    global proj_list
+    # print('in main')
     current_datetime = get_datetime()
+
     do_setup(current_datetime)
     org_name, auth_key, team_id, sleep_time, critical_threshold, warning_threshold = parse_config()
 
-    #override value in config file
-    # if(sleep_value > 0):
-    #     sleep_time = sleep_value
-
     check_org_name(auth_key, org_name)
     check_threshold(critical_threshold, warning_threshold)
-    alert_list = check_alert(org_name, auth_key)
-    create_alerts(org_name, auth_key, team_id, alert_list, sleep_time, critical_threshold, warning_threshold)
+    # alert_list = check_alert(org_name, auth_key)
+
+    data_source = 'alert-rules'
+    get_data(org_name, data_source, alert_list)
+  
+    data_source = 'projects'
+    get_data(org_name, data_source, proj_list)
+   
+    create_alerts(org_name, auth_key, team_id, sleep_time, critical_threshold, warning_threshold, proj_list, alert_list)
     
 
 if __name__ == '__main__':
